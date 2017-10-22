fb50fb4aceddf9c2273bc7ab424101ded893a01e
LSaldivia <lsaldivia@est.frba.utn.edu.ar>
2017-10-22 08:23:15 -0800

    Borrando Archivos de versiones viejos

--- inc/AlInitTodo.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_native_dialog.h>
-
-int AlDrawTodo(Objeto **, float, float);
-int AlInitTodo(ALLEGRO_TIMER **, ALLEGRO_TIMER **, ALLEGRO_DISPLAY **, ALLEGRO_EVENT_QUEUE **, Objeto **);
