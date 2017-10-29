54d0635ccd322e7af53c1eaf02cda5664d2a0b27
LucianoSaldivia <lsaldivia@est.frba.utn.edu.ar>
2017-10-29 01:38:14 -0300

    Elimino archivos viejos

--- inc/SaltosFrogLib.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_native_dialog.h>
-
-void FrogSaltaArriba(Objeto **, Objeto **, ALLEGRO_BITMAP **);
-void FrogSaltaIzquierda(Objeto **, Objeto **, ALLEGRO_BITMAP **);
-void FrogSaltaAbajo(Objeto **, Objeto **, ALLEGRO_BITMAP **);
-void FrogSaltaDerecha(Objeto **, Objeto **, ALLEGRO_BITMAP **);
