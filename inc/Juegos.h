5fc899b2fba78560f56a07e71d88ccaa4be97505
LucianoSaldivia <lsaldivia@est.frba.utn.edu.ar>
2017-11-05 18:08:24 -0300

    Algunas Imagenes faltan otras estan mal

--- inc/Juegos.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_native_dialog.h>
-#include <allegro5/allegro_font.h>
-#include <allegro5/allegro_ttf.h>
-
-char Juego1vsC( ALLEGRO_DISPLAY **p_display, ALLEGRO_EVENT_QUEUE **p_event_queue );
--- inc/MovimientoAmbiente.h
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include <allegro5/allegro.h>
-#include <allegro5/allegro_image.h>
-#include <allegro5/allegro_native_dialog.h>
-
-void MoverTodo( Objeto **p_Ini, Objeto **p_ObjetoFrog );
