bf572f67377e4ce2de156e9f1620ed20fd205da2
LucianoSaldivia <lsaldivia@est.frba.utn.edu.ar>
2017-11-13 15:45:11 -0300

    SegFault al final
    -Corregí lgunos bugs
    -Si no fuera por el SegFault del final, esta sería la version final del 1vsC

---
+++ inc/Saltos.h
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_image.h>
+#include <allegro5/allegro_native_dialog.h>
+
+void RanaSaltaArriba( Objeto **p_ObjetoFrog, ALLEGRO_BITMAP **VecFrog, char *p_ContadorSprites, char *PosicionesFinales );
+void RanaSaltaAbajo( Objeto **p_ObjetoFrog, ALLEGRO_BITMAP **VecFrog, char *p_ContadorSprites );
+void RanaSaltaIzquierda( Objeto **p_ObjetoFrog, ALLEGRO_BITMAP **VecFrog, char *p_ContadorSprites );
+void RanaSaltaDerecha( Objeto **p_ObjetoFrog, ALLEGRO_BITMAP **VecFrog, char *p_ContadorSprites );
