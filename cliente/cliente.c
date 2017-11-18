44f07e3bed0c8e5f97a993649cea12de77232154
Leandro Kollenberger <leandro.kollenberger@redb.ee>
2017-11-18 12:57:57 -0300

    primer intento de cliente

---
+++ cliente/cliente.c
@@ -0,0 +1,227 @@
+/*
+ * Basic Keyboard Example
+ * https://wiki.allegro.cc/index.php?title=Basic_Keyboard_Example
+ *         Para correrlo:
+ *             al08_keyboard_client <IP>
+ */
+
+#include <string.h>
+#include <stdio.h>
+#include <allegro5/allegro.h>
+ 
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+#include "cliente.h"
+
+int main(int argc, char **argv)
+{
+    /*************************
+     * Variables de Allegro
+     */
+
+   ALLEGRO_DISPLAY *display = NULL;
+   ALLEGRO_EVENT_QUEUE *event_queue = NULL;
+   ALLEGRO_TIMER *timer = NULL;
+   ALLEGRO_BITMAP *bouncer = NULL;
+   float bouncer_x = SCREEN_W / 2.0 - BOUNCER_SIZE / 2.0;
+   float bouncer_y = SCREEN_H / 2.0 - BOUNCER_SIZE / 2.0;
+   bool key[4] = { false, false, false, false };
+   bool redraw = true;
+   bool doexit = false;
+ 
+    /*************************
+     * Variables de Server TCP
+     */
+
+    int sockfd;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    Teclas_2 tmp = {0};
+    int puerto;
+
+    /*************************
+     * Inicializacion Server TCP
+     */
+
+    if (argc < 2) {
+       fprintf(stderr,"usage %s hostname port\n", argv[0]);
+       exit(0);
+    }
+
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+
+    server = gethostbyname(argv[1]);
+
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+
+    memset((void *) &serv_addr, '\0', sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+
+    bcopy((char *)server->h_addr, 
+         (char *)&serv_addr.sin_addr.s_addr,
+         server->h_length);
+
+    puerto = atoi(argv[2]);
+    serv_addr.sin_port = htons(puerto);
+
+    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+        error("ERROR connecting");
+
+    /*************************
+     * Inicialiación y configuración Allegro
+     */
+
+   if(!al_init()) {
+      fprintf(stderr, "failed to initialize allegro!\n");
+      return -1;
+   }
+ 
+   if(!al_install_keyboard()) {
+      fprintf(stderr, "failed to initialize the keyboard!\n");
+      return -1;
+   }
+ 
+   timer = al_create_timer(1.0 / FPS);
+   if(!timer) {
+      fprintf(stderr, "failed to create timer!\n");
+      return -1;
+   }
+ 
+   display = al_create_display(SCREEN_W, SCREEN_H);
+   if(!display) {
+      fprintf(stderr, "failed to create display!\n");
+      al_destroy_timer(timer);
+      return -1;
+   }
+ 
+   bouncer = al_create_bitmap(BOUNCER_SIZE, BOUNCER_SIZE);
+   if(!bouncer) {
+      fprintf(stderr, "failed to create bouncer bitmap!\n");
+      al_destroy_display(display);
+      al_destroy_timer(timer);
+      return -1;
+   }
+ 
+   al_set_target_bitmap(bouncer);
+   al_clear_to_color(al_map_rgb(255, 0, 255));
+   al_set_target_bitmap(al_get_backbuffer(display));
+ 
+   event_queue = al_create_event_queue();
+   if(!event_queue) {
+      fprintf(stderr, "failed to create event_queue!\n");
+      al_destroy_bitmap(bouncer);
+      al_destroy_display(display);
+      al_destroy_timer(timer);
+      return -1;
+   }
+ 
+   al_register_event_source(event_queue, al_get_display_event_source(display));
+   al_register_event_source(event_queue, al_get_timer_event_source(timer));
+   al_register_event_source(event_queue, al_get_keyboard_event_source());
+ 
+   al_clear_to_color(al_map_rgb(0,0,0));
+   al_flip_display();
+   al_start_timer(timer);
+ 
+    /*************************
+     * Listos para empezar el juego!
+     */
+
+   while(!doexit)
+   {
+      ALLEGRO_EVENT ev;
+      al_wait_for_event(event_queue, &ev);
+ 
+      if(ev.type == ALLEGRO_EVENT_TIMER) {
+         if(key[KEY_UP] && bouncer_y >= 4.0) {
+            bouncer_y -= 4.0;
+         }
+ 
+         if(key[KEY_DOWN] && bouncer_y <= SCREEN_H - BOUNCER_SIZE - 4.0) {
+            bouncer_y += 4.0;
+         }
+ 
+         if(key[KEY_LEFT] && bouncer_x >= 4.0) {
+            bouncer_x -= 4.0;
+         }
+ 
+         if(key[KEY_RIGHT] && bouncer_x <= SCREEN_W - BOUNCER_SIZE - 4.0) {
+            bouncer_x += 4.0;
+         }
+ 
+         redraw = true;
+      } else if(ev.type == ALLEGRO_EVENT_DISPLAY_CLOSE) {
+         break;
+      } else if(ev.type == ALLEGRO_EVENT_KEY_DOWN) {
+         switch(ev.keyboard.keycode) {
+            case ALLEGRO_KEY_UP:
+               tmp.T_arriba = true;
+               send(sockfd, &tmp, sizeof(tmp), MSG_DONTWAIT)
+               break; 
+            case ALLEGRO_KEY_DOWN:
+               tmp.T_abajo = true;
+               send(sockfd, &tmp, sizeof(tmp), MSG_DONTWAIT)
+               break; 
+            case ALLEGRO_KEY_LEFT: 
+               tmp.T_izquierda = true;
+               send(sockfd, &tmp, sizeof(tmp), MSG_DONTWAIT)
+               break;
+            case ALLEGRO_KEY_RIGHT:
+               tmp.T_derecha = true;
+               send(sockfd, &tmp, sizeof(tmp), MSG_DONTWAIT)
+               break;
+         }
+      } else if(ev.type == ALLEGRO_EVENT_KEY_UP) {
+         switch(ev.keyboard.keycode) {
+            case ALLEGRO_KEY_UP:
+               tmp.T_arriba = false;
+               send(sockfd, &tmp, sizeof(tmp), MSG_DONTWAIT)
+               break;
+            case ALLEGRO_KEY_DOWN:
+               tmp.T_abajo = false;
+               send(sockfd, &tmp, sizeof(tmp), MSG_DONTWAIT)
+               break;
+            case ALLEGRO_KEY_LEFT: 
+               tmp.T_izquierda = false;
+               send(sockfd, &tmp, sizeof(tmp), MSG_DONTWAIT)
+               break; 
+            case ALLEGRO_KEY_RIGHT:
+               tmp.T_derecha = false;
+               send(sockfd, &tmp, sizeof(tmp), MSG_DONTWAIT)
+               break; 
+            case ALLEGRO_KEY_ESCAPE:
+               doexit = true;
+               send(sockfd, &tmp, sizeof(tmp), MSG_DONTWAIT)
+               break;
+         }
+      }
+ 
+      if(redraw && al_is_event_queue_empty(event_queue)) {
+         redraw = false;
+
+         al_clear_to_color(al_map_rgb(0,0,0));
+         al_draw_bitmap(bouncer, bouncer_x, bouncer_y, 0);
+         al_flip_display();
+      }
+   }
+ 
+   al_destroy_bitmap(bouncer);
+   al_destroy_timer(timer);
+   al_destroy_display(display);
+   al_destroy_event_queue(event_queue);
+
+   close(sockfd);
+ 
+   return 0;
+}
---
+++ cliente/cliente.h
@@ -0,0 +1,21 @@
+#define MY_EVENT_TYPE ALLEGRO_GET_EVENT_TYPE('S','O','C','K')
+
+const float FPS = 60;
+const int SCREEN_W = 640;
+const int SCREEN_H = 480;
+const int BOUNCER_SIZE = 32;
+
+enum MYKEYS {
+   KEY_UP, KEY_DOWN, KEY_LEFT, KEY_RIGHT, KEY_EXIT
+};
+
+typedef struct Teclas_2
+{
+    bool T_arriba,T_abajo, T_izquierda, T_derecha
+} Teclas_2G;
+
+void error(const char *msg)
+{
+    perror(msg);
+    exit(0);
+}
